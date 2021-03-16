a11df9af0588617e5f3dbeaa59f38ca58bfa9c9c
Olukayode <olukayodeshokunbi@gmail.com>
2021-03-16 19:28:41 -0400

    Modified working files

--- youtube-backend/index.js
+++ youtube-backend/index.js
@@ -1,18 +1,18 @@
-const connectDB = require('./startup/db');
-const express = require('express');
+const connectDB = require("./startup/db");
+const express = require("express");
 const app = express();
-const comments = require('./routes/comments');
-const replies = require('./routes/replies');
-const cors = require('cors');
+const comments = require("./routes/comments");
+const replies = require("./routes/replies");
+const cors = require("cors");
 
 connectDB();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/comments', comments);
-app.use('/api/replies', replies);
+app.use("/api/comments", comments);
+app.use("/api/replies", replies);
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server started on port: ${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
 });
--- youtube-frontend/src/components/SearchBar/searchBar.jsx
+++ youtube-frontend/src/components/SearchBar/searchBar.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import './searchBar.css';
+import React from "react";
+import "./searchBar.css";
 
 function SearchBar(props) {
-    return (
-        <div className="row row-spacer">
-            <div className="col-md-12" style={{padding: 0}}>
-                <div className="searchbar-nav">
-                    <h1>Youtube App SearchBar</h1>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <form onSubmit={props.handleSubmit}>
+        <label>
+          <input type="search" onChange={props.handleChange}></input>
+          <i type="submit" class="fas fa-search"></i>
+        </label>
+      </form>
+    </div>
+  );
 }
+// function SearchBar(props) {
+//     return (
+//         <div className="row row-spacer">
+//             <div className="col-md-12" style={{padding: 0}}>
+//                 <div className="searchbar-nav">
+//                     <h1>Youtube App SearchBar</h1>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
 
 export default SearchBar;
