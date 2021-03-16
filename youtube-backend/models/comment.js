5669846be9d2a48246b7a9ec63f1c74080796092
chasereske <chasereske@gmail.com>
2021-03-15 20:51:30 -0400

    Created and exported both the comment model and the reply model.

---
+++ youtube-backend/models/comment.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const commentSchemma = new mongoose.Schema({
+    text: { type: String, required: true, minlength: 2, maxlength: 300 },
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number, default: 0 },
+    replies: {},
+    videoId: { type: String, required: true },
+    date: { type: Date, default: Date.now }
+})
+
+const Comment = mongoose.model('Comment', commentSchemma);
+
+module.exports = Comment; 
---
+++ youtube-backend/models/reply.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const replySchemma = new mongoose.Schema({
+    text: { type: String, Required: true, minlength: 2, maxlength: 300 },
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number, default: 0 },
+    date: { type: Date, default: Date.now }
+})
+
+
+const Reply = mongoose.model('Reply', replySchemma);
+
+module.exports = Reply;
