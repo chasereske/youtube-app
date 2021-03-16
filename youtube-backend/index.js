75264b7bae967b361b792515a04f5f4ab9f6a9f1
chasereske <chasereske@gmail.com>
2021-03-15 20:47:41 -0400

    Created the startup folders and files to initalize the backend.

--- youtube-backend/index.js
+++ youtube-backend/index.js
@@ -1,1 +1,23 @@
-const mongoose = require('mongoose');
+const connectDB = require('./startup/db');
+
+connectDB();
+
+const replySchemma = new mongoose.Schema({
+    text: { type: String, Required: true, minlength: 2, maxlength: 300 },
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number, default: 0 },
+    date: { type: Date, default: Date.now }
+})
+
+const commentSchemma = new mongoose.Schema({
+    text: { type: String, required: true, minlength: 2, maxlength: 300 },
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number, default: 0 },
+    replies: {},
+    videoId: { type: String, required: true },
+    date: { type: Date, default: Date.now }
+})
+
+const Comment = mongoose.model('Comment', commentSchemma);
+const Reply = mongoose.model('Reply', replySchemma);
+//create replySchema
