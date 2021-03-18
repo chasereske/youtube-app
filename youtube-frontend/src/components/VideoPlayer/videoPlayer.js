95316f8577e66aa0ea4cacc936985c513b33338b
Olukayode <olukayodeshokunbi@gmail.com>
2021-03-17 20:43:54 -0400

    Created VideoPlayer component that renders videopplayer iframe to screen, and plays the selected video

--- youtube-frontend/src/components/VideoPlayer/videoPlayer.js
+++ youtube-frontend/src/components/VideoPlayer/videoPlayer.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 function VideoPlayer(props) {
+  const videoId = props.videoId;
+  let videoSrc = `https://www.youtube.com/embed/${videoId}`;
   return (
     <div>
-      <div></div>
+      <iframe src={videoSrc} title={videoId}></iframe>
     </div>
   );
 }
