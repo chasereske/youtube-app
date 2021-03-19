c433ed11ee6a252a031203def85881523328661e
Olukayode <olukayodeshokunbi@gmail.com>
2021-03-19 08:27:48 -0400

    Created  GET endpoint and handler that returns the comments from selected video if it exists in the database

--- youtube-backend/routes/videos.js
+++ youtube-backend/routes/videos.js
@@ -1,16 +1,22 @@
 const { Video } = require("../models/video");
-const { Comment } = require("../models/comment");
-const Reply = require("../models/reply");
+const { Comment, validateComment } = require("../models/comment");
+const { Reply, validateReply } = require("../models/reply");
 const express = require("express");
 const router = express.Router();
 
 // GET COMMENTS ASSOCIATED WITH SPECIFIC VIDEO
 router.get("/:videoId", async (req, res) => {
   try {
+    // CHECK IF VIDEO INFO EXISTS IN DATABASE
     const videoId = req.params.videoId;
-    const video = await Video.find({ videoId: videoId });
+    const videoArr = await Video.find({ videoId: videoId });
 
-    return res.send(video);
+    if (!videoArr) return res.status(400).send("Not Found");
+
+    // DESTRUCTURE THE SINGLE OBJECT FROM RETURNED ARRAY
+    const [video] = videoArr;
+
+    return res.send(video.comments);
   } catch (error) {
     res.status(500).send(error);
   }
@@ -19,6 +25,9 @@ router.get("/:videoId", async (req, res) => {
 // POST COMMENT TO SELECTED VIDEO
 router.post("/:videoId", async (req, res) => {
   try {
+    // const videoId = req.params.videoId;
+    // const video = await Video.find({ videoId: videoId });
+
     const video = new Video({
       videoId: req.body.videoId,
     });
