eb70d1068a4b8eb747ca55ee6010f11e8b9a2d26
Olukayode <olukayodeshokunbi@gmail.com>
2021-03-22 08:31:58 -0400

    Added some styling to the search results

--- youtube-frontend/src/components/VideoSearchResult/videoSearchResult.js
+++ youtube-frontend/src/components/VideoSearchResult/videoSearchResult.js
@@ -1,20 +1,22 @@
 import React from "react";
+import "./videoSearchResult.css";
 
 function VideoSearchResult(props) {
   if (props.videoResults) {
     const videoResult = props.videoResults.map((el, index) => {
       return (
-        <div key={index} className="mx-auto">
+        <div key={index} className="mx-auto float">
           <img
-            className=""
+            className="search-result-thumbnail"
             src={el.snippet.thumbnails.medium.url}
             alt={el.snippet.description}
             onClick={() => props.selectVideoToPlay(el)}
           ></img>
-          <div className="">
+          <div className="title-description">
             <div>{el.snippet.title}</div>
             <div>{el.snippet.description}</div>
           </div>
+          <hr></hr>
         </div>
       );
     });
